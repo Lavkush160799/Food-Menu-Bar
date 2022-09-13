321092fd3e69ae8e8756ad190c12b860d8cce208
Lavkush Yadav <101989830+Lavkush160799@users.noreply.github.com>
2022-09-13 22:09:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Resturant from './component/Resturant';
+import Footer from './Footer'
+function App() {
+  return (
+    <>
+    <Resturant/>
+  <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+  <>
+  <h1>This is footer </h1>
+  </>
+  )
+}
+
+export default Footer;
